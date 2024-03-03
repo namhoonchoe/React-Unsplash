1b95377f17e6c42f0942d8cc20f251d01b56f8fb
Namhoon Choe <sysynh2012@gmail.com>
2024-03-03 19:20:36 +0900

    [Fix] 타입배럴 수정
    타입배럴 수정 중....

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -1,13 +1,10 @@
-export * from "./collection"
-export * from "./collectionResult"
-export * from "./photo"
-export * from "./photoDetail"
-export * from "./photoResults"
-export * from "./topic"
-export type { topicPhoto } from "./topicPhoto"
-export type { User } from "./user"
-export type { UserResult } from "./userResult"
-
-
-
+export type { CollectionInfo } from "./collection";
+export type { CollectionResult } from "./collectionResult";
+export type { Photo } from "./photo";
+export type { PhotoDetail } from "./photoDetail";
+export type { PhotoResults } from "./photoResults";
+export type { Topic } from "./topic";
+export type { topicPhoto } from "./topicPhoto";
+export type { User } from "./user";
+export type { UserResult } from "./userResult";
 
